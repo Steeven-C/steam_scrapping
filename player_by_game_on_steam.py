4ac7a9bd4e47932e7fcf42a7efcfe3e027a24b9e
steeven camus <steevencamus@gmail.com>
2020-10-01 14:15:54 +0200

    add game_genre

--- player_by_game_on_steam.py
+++ player_by_game_on_steam.py
@@ -12,8 +12,15 @@ soup = BeautifulSoup(r.text,"html.parser")
 game_activities = soup.find_all("tr",class_="player_count_row")
 for game_activity in game_activities:
     game_activity = game_activity.text
-    print(game_activity)
+    #print(game_activity)
 
 
 steam_player = soup.find("span", class_="statsTopHi").text
-print(steam_player)
+#print(steam_player)
+
+s = requests. get("https://store.steampowered.com/app/730/CounterStrike_Global_Offensive/")
+souping = BeautifulSoup(s.text,"html.parser")
+
+game_genre = souping.find("a",class_="app_tag").text
+game_genre = game_genre.strip()
+print(game_genre)
